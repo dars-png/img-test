c0873453e88d7c177c161fa15e40388c93e62041
dars-png <rasda103@student.liu.se>
2026-03-09 22:12:57 +0100

    Updated test to use fixed version of img.h

--- include/img.h
+++ include/img.h
@@ -1,10 +1,20 @@
 #ifndef IMG_H
 #define IMG_H
 
-#include <stdint.h>
-#include "sokol_gfx.h"
-#include "stb_image.h"
+// okay we're gonna make this bihh header-only
 
+#include <stdint.h>
+#include <unistd.h>
+
+#ifndef SOKOL_GFX_INCLUDED
+#include "../sokol/sokol_gfx.h"
+#endif
+
+#ifdef STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#endif
+
+// wow it's a struct
 typedef struct img_data {
 	int width;	
 	int height;	
@@ -13,37 +23,52 @@ typedef struct img_data {
 	unsigned char* data;
 } img_data;
 
-//static inline img_data load_img_data_from_file(const char* path);
-//static inline sg_image load_img_from_data(img_data data);
+img_data load_img_data_from_file(const char* path);
+void load_img_from_data(sg_image img, img_data data);
+void img_data_free(img_data data);
 
-static inline img_data load_img_data_from_file(const char *path) {
+
+#ifdef DARS_IMG_IMPLEMENTATION
+img_data load_img_data_from_file(const char* path) {
 	img_data data;
+
+	char cwd[1024];
+	getcwd(cwd, sizeof(cwd));
+	printf("Working Directory: %s\n", cwd);
+	printf("Given Path: %s\n", path);
+
+	stbi_set_flip_vertically_on_load(1);
 	data.data = stbi_load(path, &data.width, &data.height, &data.channels, 4);
+	if (!data.data) {
+		printf("ERROR LOADING IMAGE FILE: %s\n", stbi_failure_reason());
+		exit(-2);
+	}
 	return data;
 }
 
-static inline sg_image load_img_from_data(img_data data) {
+void load_img_from_data(sg_image img, img_data data) {
 	if (!data.data) {
-		return (sg_image){0};
+		return;
 	}
 
-	sg_image img = sg_alloc_image();
+	printf("DATA FOR IMAGE\n");
+	printf("Width: %d\n", data.width);
+	printf("Height: %d\n", data.height);
 
 	sg_init_image(img, &(sg_image_desc){
+		.type = SG_IMAGETYPE_2D,
 		.width = data.width,
 		.height = data.height,
-		.pixel_format = SG_PIXELFORMAT_RGBA8,
-		.usage.immutable = true,
-		.sample_count = 1,
-		.data = (sg_range) {
+		.data.mip_levels[0] = {
 			.ptr = data.data,
-			.size = (size_t) (data.width * data.height * 4)
-		}
+			.size = (size_t) (data.width * data.height * 4)},
+		.pixel_format = SG_PIXELFORMAT_RGBA8,
 			});
-
-
-	return img;
 }
 
+void img_data_free(img_data data) {
+	stbi_image_free(data.data);
+}
+#endif
 
 #endif
--- test.c
+++ test.c
@@ -1,11 +1,14 @@
 #define SOKOL_IMPL
 #define SOKOL_GLCORE
-#include "img.h"
+#include "sokol/sokol_gfx.h"
 #include "sokol/sokol_app.h"
 #include "sokol/sokol_glue.h"
 #include "sokol/sokol_log.h"
 #include "shaders/test.glsl.h"
 
+#define DARS_IMG_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#include "include/img.h"
 
 static struct {
 	sg_pipeline pip;
